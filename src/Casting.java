b662236ed2b4fdc6a8f944c3ada1ee91455800a2
Miguel Angel Sierra Bolaños <90860745+SMiguelA@users.noreply.github.com>
2026-06-16 18:51:43 -0500

    Various

---
+++ src/Casting.java
@@ -0,0 +1,7 @@
+public class Casting {
+    public static void main(String[] args) {
+        long numL = 25000000000000L;
+        int numC = (int)numL;
+        System.out.println(numC);
+    }
+}
---
+++ src/DuplicatedNum.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class DuplicatedNum {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>(List.of( 7, 1, 2, 2, 6, 3, 4, 5, 6, 7, 8, 9));
+        List<Integer> duplicatedOnesArr = DuplicatedNum.duplicatedOnes(list);
+        System.out.println(duplicatedOnesArr);
+    }
+
+    public static List<Integer> duplicatedOnes(List<Integer> arr) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int valor: arr){
+            map.put(valor,map.getOrDefault(valor,0)+1);
+        }
+        System.out.println(map);
+        List<Integer> list = new ArrayList<>();
+        for (int valor: map.keySet()){
+            if(map.get(valor)>1){
+                list.add(valor);
+            }
+        }
+        return list;
+    }
+}
