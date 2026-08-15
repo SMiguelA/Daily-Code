e3edf2857eb566270dfd55e7ceb1b479a76b349f
Miguel Angel Sierra Bolaños <90860745+SMiguelA@users.noreply.github.com>
2026-08-14 21:15:53 -0500

    Fixing the count function adding a range between left and right

--- src/Basic_Exercises/Ocurrences.java
+++ src/Basic_Exercises/Ocurrences.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 public class Ocurrences {
     public static void main(String[] args) {
-        System.out.println(findLength("bcbbbcba"));
+        System.out.println(findSecuence("aaaa"));
     }
     // Maximum Length Substring With Two Occurrences
     // Given a string s, return the maximum length of a substring such that it contains at most two occurrences of each character.
@@ -23,6 +23,29 @@ public class Ocurrences {
                 count = 0;
             }
             if(count > maxCount) maxCount=count;
+        }
+        return maxCount;
+    }
+
+    // No funciona, podria conciderar usar solo un array y no una hash table, usando dos punteros en un array
+    // Retornar el numero de caracteres en una secuencia donde solo se pueden repetir dos letras
+
+    public static int findSecuence(String str){
+        int left=0;
+        int maxCount=0;
+        final String[] strArr = str.split("");
+        final Map<String,Integer> map=new HashMap<>();
+        for(int right=0;right <strArr.length;right++){
+            map.put(strArr[right], map.getOrDefault(strArr[right],0)+1);
+            while(map.get(strArr[right])>2){
+                int leftVal = map.get(strArr[left])-1;
+                map.put(strArr[left], leftVal);
+                if(map.get(strArr[left]) == 0){
+                    map.remove(strArr[left]);
+                }
+                left++;
+            }
+            maxCount = Math.max(maxCount, right - left +1);
         }
         return maxCount;
     }
